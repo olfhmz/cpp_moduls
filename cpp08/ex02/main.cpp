a368b627bcf34cde78f650d933257299421e2d28
Aemma Dyan <aedyan@un-e6.kzn.21-school.ru>
2022-08-17 06:19:28 +0300

    cpp08 done

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,27 +1,76 @@
 #include "MutantStack.hpp"
+#include <iostream>
+#include <vector>
 
-int main()
+void	testVector(void)
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	std::vector<int>	mstack;
+
+	mstack.push_back(5);
+	mstack.push_back(17);
+
+	std::cout << "top = " << *(mstack.end() - 1) << std::endl;
+	
+	mstack.pop_back();
+
+	std::cout << "size = " << mstack.size() << std::endl;
+
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	mstack.push_back(-12);
+	mstack.push_back(0);
+
+	std::vector<int>::iterator	it = mstack.begin();
+	std::vector<int>::iterator	ite = mstack.end();
+
 	++it;
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
-	std::stack<int> s(mstack);
-	return 0;
+}
+
+void	testSubject(void)
+{
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "top = " << mstack.top() << std::endl;
+	
+	mstack.pop();
+
+	std::cout << "size = " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(-12);
+	mstack.push(0);
+
+	MutantStack<int>::iterator	it = mstack.begin();
+	MutantStack<int>::iterator	ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	
+	std::stack <int>	s(mstack);
+}
+
+int	main(void)
+{
+	testSubject();
+	std::cout << " ------ --- ------ " << std::endl;
+	testVector();
+	
+	return (0);
 }
